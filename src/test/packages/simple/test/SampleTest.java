a399e4fa6fbd783b74eab5abf641512cafb5789a
zsellera <zsellera@gmail.com>
2012-03-15 19:40:23 +0100

    test folder updates

---
+++ src/test/packages/simple/test/SampleTest.java
@@ -0,0 +1,14 @@
+package simple.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SampleTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
